b1aa2f85b89ae4d53a24b6fcbe2f406413a5934b
MAGHC <windowsapple95@gmail.com>
2023-01-27 21:55:24 +0900

    fix: Product Skeleton style 마진값 미세 조정

--- src/components/skeleton/ProductSkeleton.tsx
+++ src/components/skeleton/ProductSkeleton.tsx
@@ -1,8 +1,8 @@
 const ProductSkeleton = () => {
   return (
-    <li className=" animate-pulse mx-8">
+    <li className=" animate-pulse mx-12">
       <div className=" bg-slate-400 w-44 h-40"></div>
-      <div className="my-4 gap-4 flex justify-between">
+      <div className="my-2 gap-4 flex justify-between">
         <div className=" bg-slate-400 w-3/4 h-3"></div>
         <div className=" bg-slate-400 w-1/4 h-3"></div>
       </div>
