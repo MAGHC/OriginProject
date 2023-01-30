dec0dc1889153c667f1a181c996f15bcdcfbdfdd
MAGHC <windowsapple95@gmail.com>
2023-01-30 21:17:27 +0900

    style: 디자인 수정에따른 ProductCard round값 추가

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -12,7 +12,11 @@ const ProductCard = ({ product, product: { id, img, title, price } }: PropsI) =>
       className=" flex flex-col items-center"
       onClick={() => navigate(`/product/${id}`, { state: { product } })}
     >
-      <img className=" cursor-pointer w-56 h-44 object-cover" alt={title} src={img}></img>
+      <img
+        className=" rounded-lg cursor-pointer w-56 h-44 object-cover"
+        alt={title}
+        src={img}
+      ></img>
       <div className=" w-40 md:w-44  lg:w-48 xl:w-56  flex justify-between mx-auto   ">
         <h3 className=" truncate ... w-1/2">{title}</h3>
         <p className=" truncate ... font-semibold flex ">{`₩${price}`}</p>
--- src/components/skeleton/ProductSkeleton.tsx
+++ src/components/skeleton/ProductSkeleton.tsx
@@ -1,7 +1,7 @@
 const ProductSkeleton = () => {
   return (
     <li className=" animate-pulse mx-auto ">
-      <div className=" bg-slate-400 w-56 h-44"></div>
+      <div className=" rounded-lg bg-slate-400 w-56 h-44"></div>
       <div className=" w-56 my-2 gap-4 flex justify-between">
         <div className=" bg-slate-400 w-1/4 h-3"></div>
         <div className=" bg-slate-400 w-1/4 h-3"></div>
