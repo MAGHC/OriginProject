81016847d2de931ded1f656ec6285d948ea34a40
MAGHC <windowsapple95@gmail.com>
2023-02-03 17:26:33 +0900

    feat:카테고리별 정렬기능구현

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,8 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 
+import { useState } from 'react';
+
 import { getProducts } from '../api/firebase.js';
 import ProductCard from './ProductCard';
 import ProductSkeleton from './skeleton/ProductSkeleton';
+
+import { GetProductTypeI } from './../type/GetProductType';
 
 const PRODUCT_WRAPPER = ' md:mx-[16rem]  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8';
 
@@ -12,14 +16,25 @@ MockSkeletonState.fill(null);
 const CATEGORYIES = ['Women', 'Goods', 'Life', 'Special'];
 
 const Products = () => {
-  const { isLoading, data: products, error } = useQuery(['products'], getProducts);
+  const [filter, setFilter] = useState<string | boolean>(false);
+
+  const {
+    error,
+    isLoading,
+    data: products,
+  } = useQuery<GetProductTypeI[]>(['products'], getProducts, {
+    select: (products) =>
+      products.filter((product) => {
+        return filter ? product.category === filter : product;
+      }),
+  });
 
   return (
     <>
       <ul className="sm:my-20 mx-[8rem] text-xl md:mx-[16rem] font-body text-highLight mt-[6.85rem] mb-[1.9rem] flex gap-12">
         {CATEGORYIES.map((category) => (
           <li className=" hover:scale-110 transition-all duration-200">
-            <button>{category}</button>
+            <button onClick={() => setFilter(category)}>{category}</button>
           </li>
         ))}
       </ul>
--- src/type/GetProductType.ts
+++ src/type/GetProductType.ts
@@ -5,4 +5,5 @@ export interface GetProductTypeI {
   price: number;
   option: Array<string | number>;
   description: string;
+  category: string;
 }
