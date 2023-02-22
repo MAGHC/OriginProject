5e52777dc1f98d9830674ea3899b6818b352d501
MAGHC <windowsapple95@gmail.com>
2023-02-22 21:20:46 +0900

    test: uitls 전체 context uitl 함수 구현

--- src/tests/utils.tsx
+++ src/tests/utils.tsx
@@ -1,4 +1,6 @@
 import { MemoryRouter, Routes } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { AuthContext, ContextI } from './../context/AuthContext';
 
 export function withRouter(routes: React.ReactElement, initalEntry = ['/']) {
   return (
@@ -6,4 +8,28 @@ export function withRouter(routes: React.ReactElement, initalEntry = ['/']) {
       <Routes>{routes}</Routes>
     </MemoryRouter>
   );
+}
+
+export function withAllContext(children: React.ReactNode, auth: ContextI) {
+  const testClient = createTestQueryClient();
+  return (
+    <QueryClientProvider client={testClient}>
+      <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>
+    </QueryClientProvider>
+  );
+}
+
+function createTestQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+    logger: {
+      log: console.log,
+      warn: console.warn,
+      error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
+    },
+  });
 }
