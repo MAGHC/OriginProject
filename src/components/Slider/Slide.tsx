9546a99d7cac95c8f31107447f119db77410986f
MAGHC <windowsapple95@gmail.com>
2023-03-07 20:15:00 +0900

    migration : slide 컴포넌트 imgae 변경

--- src/components/Slider/Slide.tsx
+++ src/components/Slider/Slide.tsx
@@ -1,12 +1,14 @@
+import Image from 'next/dist/client/image';
+
 const Slide = ({ img, index, cur }: { img: string; cur: number; index: number }) => {
   return (
-    <img
+    <Image
       className={`  ${
         cur === index ? 'opacity-100' : 'opacity-0'
       } absolute bottom-0 transition-all duration-500   w-full h-full `}
       src={img}
       alt="slide"
-    ></img>
+    ></Image>
   );
 };
 
