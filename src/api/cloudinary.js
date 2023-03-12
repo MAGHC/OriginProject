9e71fdd4165ac617338b7e5d8ff322d590333fbd
MAGHC <windowsapple95@gmail.com>
2023-03-12 18:28:51 +0900

    migration : cloudinary 환경변수 변경

--- src/api/cloudinary.js
+++ src/api/cloudinary.js
@@ -1,9 +1,9 @@
 export async function uploadImg(file) {
   const data = new FormData();
   data.append('file', file);
-  data.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET);
+  data.append('upload_preset', process.env.NEXT_PUBLIC_CLOUDINARY_PRESET);
 
-  return await fetch(process.env.REACT_APP_CLOUDINARY_URL, {
+  return await fetch(process.env.NEXT_PUBLIC_CLOUDINARY_URL, {
     method: 'POST',
     body: data,
   })
